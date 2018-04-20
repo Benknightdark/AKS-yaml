e2fed790bb6f7e4c7914d055bc9071065a1311d4
BenKung <moon791017@gmail.com>
2018-04-20 13:54:18 +0800

    add nginx-pod.yaml

--- CreateAKS.sh
+++
@@ -1,9 +0,0 @@
-az login
-
-az group create --name benAKSGroup1 --location centralus 
-
-az aks create --resource-group benAKSGroup1 --name bkhub-cluster --node-count 1 --node-vm-size Standard_D1_v2 --dns-name-prefix benAKSGroup1 --kubernetes-version=1.8.1 --generate-ssh-keys 
-
-az aks get-credentials --resource-group benAKSGroup1 --name bkhub-cluster 
-
-az aks delete --name  bkhub-cluster --resource-group benAKSGroup1 --yes --no-wait
